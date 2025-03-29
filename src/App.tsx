1d087a1a3216c7075f9e6db8f05d423b38247032
reiuu <ryn.mlsl@gmail.com>
2025-03-29 01:34:06 +0000

    further cleanup; fix frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
+// struct MidiDevice
+type MidiDevice = {
+  id: number;
+  d_name: string;
+};
+
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [devices, setDevices] = useState<MidiDevice[]>([]); // state to hold device list
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("get_midi_devices", { name }));
-  }
-
-  async function test_lib() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("test_lib", { name }));
+  async function getMidiDevices() {
+    const result = await invoke<MidiDevice[]>("get_midi_devices");
+    setDevices(result); // store it in state
   }
 
   return (
     <main>
       <h1>Welcome to Tauri + React</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
 
       <form
         onSubmit={(e) => {
-          e.preventDefault();
-          test_lib();
+          e.preventDefault(); // prevent page reload
+          getMidiDevices();
         }}
       >
-        <button type="submit">Test Lib</button>
+        <button type="submit">List MIDI devices</button>
       </form>
-      <p>{greetMsg}</p>
 
+      <ul>
+        {devices.map((device) => (
+          <li key={device.id}>
+            #{device.id}: {device.d_name}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
